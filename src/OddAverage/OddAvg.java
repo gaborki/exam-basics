f2b0ba4cd49011ab08dba3c79e45b83506e3e045
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-04-18 09:33:25 +0200

    create MyExam project, create OddAverage and Copy package

---
+++ src/OddAverage/OddAvg.java
@@ -0,0 +1,24 @@
+package OddAverage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Gabor on 18/04/2017.
+ */
+public class OddAvg {
+
+  public static void main(String[] args) {
+
+
+    }
+
+  public static double oddAverage(List<Integer> numbers){
+
+
+    // Create a function called `oddAverage` that takes a list of numbers as parameter
+    // and returns the average value of the odd numbers in the list
+    // Create basic unit tests for it with at least 3 different test cases
+  }
+}
+
